4afd53e8c307060b08e34adea29a817f3fd928fe
Humberto Gutierrez <lizard.machorro@gmail.com>
2017-01-22 18:27:14 -0500

    Add: macOS instalation scripts

---
+++ scripts/brew.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle install
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Remove outdated versions from the cellar
+brew cleanup
+brew cask cleanup

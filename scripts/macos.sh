c55c26b59792383a0b52e8ed12d7fa8b91a8b685
Humberto Gutierrez <humbertog@stradigi.ai>
2018-09-05 14:19:02 -0400

    remove + from script to kill apps

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -706,25 +706,25 @@ defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-+for app in "Activity Monitor" \
-+	"Address Book" \
-+	"Calendar" \
-+	"cfprefsd" \
-+	"Contacts" \
-+	"Dock" \
-+	"Finder" \
-+	"Google Chrome Canary" \
-+	"Google Chrome" \
-+	"Mail" \
-+	"Messages" \
-+	"Photos" \
-+	"Safari" \
-+	"SystemUIServer" \
-+	"Terminal" \
-+	"Transmission" \
-+	"Tweetbot" \
-+	"Twitter" \
-+	"Calendar"; do
+for app in "Activity Monitor" \
+	"Address Book" \
+	"Calendar" \
+	"cfprefsd" \
+	"Contacts" \
+	"Dock" \
+	"Finder" \
+	"Google Chrome Canary" \
+	"Google Chrome" \
+	"Mail" \
+	"Messages" \
+	"Photos" \
+	"Safari" \
+	"SystemUIServer" \
+	"Terminal" \
+	"Transmission" \
+	"Tweetbot" \
+	"Twitter" \
+	"Calendar"; do
  	killall "${app}" &> /dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."

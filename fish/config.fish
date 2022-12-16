2b00c89d0cec82547cbfe01241d90f588ad6cc18
Humberto Gutierrez <lizard.machorro@gmail.com>
2022-12-16 10:51:31 -0500

    Update fish config to use 1Password with Sentry token

--- fish/config.fish
+++ fish/config.fish
@@ -47,3 +47,4 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 fish_vi_key_bindings
 
 status --is-interactive; and rbenv init - fish | source
+source /Users/hgutierrez/.config/op/plugins.sh
